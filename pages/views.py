c6cb47c988fa69bfd7121fa42f4a1040f0dd9c93
Oleksandr <o.tymoshenko@astoundcommerce.com>
2024-05-18 21:03:05 +0300

    update text in main endpoint

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home_page_view(request):
-    return HttpResponse("Hello from the Platform.sh project!!!")
+    return HttpResponse("Hello from the Upsun project!!!")
